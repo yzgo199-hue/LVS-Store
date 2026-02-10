8b9ef9750780ee3dbb6b09d03cb2a9ebee6df5a0
LVS <yzgo199@gmail.com>
2026-02-10 14:00:19 +0800

    Fix import path for getAnalytics in firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCCGNmO60DLskMd4-CeOTA_mZ6qHoAfGTM",
