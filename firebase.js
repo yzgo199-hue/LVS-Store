18b5e2a9a194b340b0a355a51041a9ac3de2d1e8
LVS <yzgo199@gmail.com>
2026-02-10 11:09:26 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCCGNmO60DLskMd4-CeOTA_mZ6qHoAfGTM",
@@ -12,6 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 export default app;
